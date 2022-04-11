6f59bb3400d8c70c0a4e27daa929e32e09a52d06
hjkim88 <firadazer@gmail.com>
2022-04-11 17:43:03 -0500

    runApp() function of the Shiny app.

---
+++ R/runApp.R
@@ -0,0 +1,13 @@
+###
+#   File name : runApp.R
+#   Author    : Hyunjin Kim
+#   Date      : Apr 11, 2021
+#   Email     : hyunjin.kim@stjude.org
+#   Purpose   : Start running the Shiny app
+###
+
+library(shiny)
+
+writeLines(paste("St Jude Biohackathon Shiny App Starting..."))
+
+runApp("./R")
