173437815147647c6d49ac7dd174914e86f830ec
Cody Ramirez <13905892+CodyRamirez@users.noreply.github.com>
2022-04-12 15:30:22 -0500

    updates

--- R/runApp.R
+++ R/runApp.R
@@ -5,9 +5,10 @@
 #   Email     : hyunjin.kim@stjude.org
 #   Purpose   : Start running the Shiny app
 ###
+setwd("~/Downloads/SJHack2022_Project3-main/R")
 
 library(shiny)
 
 writeLines(paste("St Jude Biohackathon Shiny App Starting..."))
 
-runApp("./R")
+runApp("../R")
